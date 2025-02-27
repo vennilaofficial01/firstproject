08fda62ca2ede43b53be23045a1a9c55b3a7fc66
vennilaofficial01 <vennilaofficial.01@gmail.com>
2025-02-27 16:20:36 +0530

    testjavaprojectbyven

---
+++ src/com/testj/ReverseString.java
@@ -0,0 +1,26 @@
+package com.testj;
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+                                                    // Create a Scanner object for user input
+        Scanner scanner = new Scanner(System.in);
+    	
+                                                    // Prompt the user to enter a string
+        System.out.print("Enter a string to reverse: ");
+        String input = scanner.nextLine();
+
+                                                  // Reverse the string
+       
+        String reversed = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
+
+                                                    // Display the reversed string
+        System.out.println("Reversed string: " + reversed);
+
+                                                   // Close the scanner
+       scanner.close();
+    }
+}
